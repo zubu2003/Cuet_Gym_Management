452f0383529c95f771ca899dbd00c454836fdd8c
Zubayer Muntasir <zubayer2003.coc@gmail.com>
2026-04-23 17:01:29 +0600

    compare pass function solved

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -9,6 +10,32 @@ const userSchema = new mongoose.Schema({
   studentName: { type: String },    // Student name
   createdAt: { type: Date, default: Date.now }
 });
+
+userSchema.pre('save', async function save(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function comparePassword(candidatePassword) {
+  if (!this.password) {
+    return false;
+  }
+
+  // Backward compatibility for old plain-text records.
+  if (!this.password.startsWith('$2')) {
+    return this.password === candidatePassword;
+  }
+
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 
 module.exports = mongoose.model('User', userSchema);
